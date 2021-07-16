f3d7324315e0821e714ece752778a02a55f5a1b1
Constantino Samuel Alarcon Mery <constantino.alarcon@sii.cl>
2021-07-15 23:24:38 -0400

    cambios

---
+++ src/main/java/sii/arq/testk8shelloworld/TestK8sHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package sii.arq.testk8shelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestK8sHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestK8sHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sii/arq/testk8shelloworld/TestK8sHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package sii.arq.testk8shelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestK8sHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
